af6121bc09dc29db0fe001f8dda022a01e5b7be2
SzafraniecSebastian <seba40404@gmail.com>
2018-09-23 20:21:10 +0200

    Mgroup Gift Webpage

---
+++ src/js/appOffer.js
@@ -0,0 +1,87 @@
+$(function() {
+  console.log("done");
+  // $('.logo').addClass('hide').fadeIn();
+
+  //hamburger menu
+  $(".hamburger").click(function() {
+    this.classList.toggle("change");
+    $(".mobile").toggleClass("active");
+  });
+
+  //animacje
+  $('*[data-animate]').addClass('hide').each(function() {
+    $(this).viewportChecker({
+      classToAdd: 'show animated ' + $(this).data('animate'),
+      classToRemove: 'hide',
+      offset: '30%'
+    });
+  });
+
+  //pojawienie sie logo i zmiana tała menu po scrollowaniu
+  $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 10) {
+      $('.logo').fadeIn();
+      $('.main').addClass('header-down');
+    } else {
+      $('.logo').fadeOut();
+      $('.main').removeClass('header-down');
+    }
+  });
+
+  //dodanie data-set w widoku mobilnym do menu - nie działa
+  function sprawdzRozmiarOkna() {
+    console.log("aa");
+    if ($(window).width() < 996) {
+      // $('.mobile').data('animate','fadeInUp');
+      $('.mobile').data('animate','fadeInUp');
+    } else {
+      $('.mobile').data('animate','');
+    }
+  }
+  $(window).resize(sprawdzRozmiarOkna);
+
+
+  // OPACITY OF BUTTON SET TO 0%
+$(".roll").css("opacity","0");
+
+// ON MOUSE OVER
+$(".roll").hover(function () {
+
+// SET OPACITY TO 70%
+$(this).stop().animate({
+opacity: .7
+}, "slow");
+},
+
+// ON MOUSE OUT
+function () {
+
+// SET OPACITY BACK TO 50%
+$(this).stop().animate({
+opacity: 0
+}, "slow");
+});
+
+// Option slider - nie dziala
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+$('.owl-dots').hide();
+
+
+});
